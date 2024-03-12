de21663fcc3f08b48a48bd708b6ae1ac1add0096
rafaelhwagner <rafaelhwagner135@gmail.com>
2024-03-12 19:20:24 -0300

    :tada: inicial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a img
+
+  if (html.classList.contains("light")) {
+    //se tiver light mode, adicionar img light
+    img.setAttribute("src", "./Assets/mosh.png")
+  } else {
+    img.setAttribute("src", "./Assets/wig.png")
+    //se tiver sem light mode, manter a img normal
+  }
+}
